121d115775c95e266326fe6a34d82cf9a801f6ef
yuki <yuki.onishi1129@gmail.com>
2021-08-10 18:16:05 +0900

    add: babel

--- webpack.config.js
+++ webpack.config.js
@@ -8,4 +8,22 @@ module.exports = {
     path: path.join(__dirname, "dist", "assets"),
     filename: "bundle.js",
   },
+  module: {
+    rules: [
+      // test: 処理対象となるファイルを正規表現で指定 (.jsのファイルを全てbabel-loaderで処理する)
+      // exclude: 処理の対象から除外するファイル (node_modulesを除外)
+      { test: /\.js$/, exclude: /(node_modules)/, loader: "babel-loader" },
+      // cssをバンドルする
+      // style-loader: CSSをHTMLのheadタグ内にインラインスタイルを出力するloader
+      // cssをJSに変換するloader
+      // https://chaika.hatenablog.com/entry/2020/10/22/083000
+      // https://ics.media/entry/17376/
+      {
+        test: /\.css$/i,
+        //   処理の順番は後ろから
+        //   css-loaderでcssをjsに変換 → style-loaderでjsとして読み込んだcssをhtmlのインラインスタイルとして導入
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
