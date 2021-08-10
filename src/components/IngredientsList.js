72de8ae78095fbf2058b63198d39e618965fb790
yuki <yuki.onishi1129@gmail.com>
2021-08-10 18:24:16 +0900

    update: repair

--- src/components/IngredientsList.js
+++ src/components/IngredientsList.js
@@ -2,11 +2,11 @@ import React from "react";
 /* components */
 import Ingredient from "./Ingredient";
 
-export default function IngredientList({ list }) {
+export default function IngredientsList({ list }) {
   return (
     <ul className="ingredients">
       {list.map((ingredient, i) => (
-        <Ingredient key={i} {...map.ingredient} />
+        <Ingredient key={i} {...ingredient} />
       ))}
     </ul>
   );
